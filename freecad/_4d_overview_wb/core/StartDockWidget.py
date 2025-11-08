0bd776cb8c74193a1c0fa2675b113731c8e38582
GA3D <ga3d.tech2021@gmail.com>
2025-11-08 18:51:08 +0100

    Project folder selection & check of 4DOverview folder structure

--- freecad/_4d_overview_wb/core/StartDockWidget.py
+++ freecad/_4d_overview_wb/core/StartDockWidget.py
@@ -1,13 +1,58 @@
 from PySide6 import QtWidgets, QtCore
 import FreeCADGui
+import os
 
 class FourOverviewMainPanel(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("4D Overview - Main")
         layout = QtWidgets.QVBoxLayout(self)
+
+        # --- Project folder selection ---
+        toplineQW = QtWidgets.QHBoxLayout()
+        self.folderLabel = QtWidgets.QLabel("Project Folder:")
+        self.folderPath = QtWidgets.QLineEdit()
+        self.browseBtn = QtWidgets.QPushButton("browse")
+        self.browseBtn.clicked.connect(self.selectFolder)
+        toplineQW .addWidget(self.folderLabel)
+        toplineQW .addWidget(self.folderPath)
+        toplineQW .addWidget(self.browseBtn)
+        layout.addLayout(toplineQW)
+
+
+
+
+
+        # --- Test Text ---
         label = QtWidgets.QLabel("Bienvenue dans 4D Overview")
         layout.addWidget(label)
+
+
+
+
+
+        #---------------GUI Prepa END ------------------#
+
+    # --- 
+    def selectFolder(self):
+        path = QtWidgets.QFileDialog.getExistingDirectory(self, "Choose a folder")
+        if path:
+            self.folderPath.setText(path)
+            self.checkFolderStructure4D(path)
+
+
+    # ---
+    def checkFolderStructure4D(self, root):
+        """Create the 4DOverview files Structures : ./4DOverview/.FileName"""
+        overview = os.path.join(root, "4DOverview")
+        if not os.path.exists(overview):
+            os.makedirs(overview)
+        for f in os.listdir(root):
+            if f.endswith(".FCStd"):
+                name = os.path.splitext(f)[0]
+                sub = os.path.join(overview, f".{name}")
+                if not os.path.exists(sub):
+                    os.makedirs(sub)
         
 
 def start () :

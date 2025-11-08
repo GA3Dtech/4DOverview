5401bc488c609e76ada65ec62c5f6f05e20e1d1d
GA3D <ga3d.tech2021@gmail.com>
2025-11-08 22:15:11 +0100

    init CentralWindowOverview

--- freecad/_4d_overview_wb/core/CentralWindowOverview.py
+++ freecad/_4d_overview_wb/core/CentralWindowOverview.py
@@ -6,11 +6,12 @@ class CentralView(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         layout = QtWidgets.QVBoxLayout(self)
-        label = QtWidgets.QLabel("<h2>Overview Generic Window</h2>")
-        button = QtWidgets.QPushButton("Click me")
+        label = QtWidgets.QLabel("<h2>Project Overview</h2>")
+       #button = QtWidgets.QPushButton("Click me")
         layout.addWidget(label)
-        layout.addWidget(button)
-        button.clicked.connect(lambda: label.setText("Hello :)"))
+        #layout.addWidget(button)
+        #button.clicked.connect(lambda: label.setText("Hello :)"))
+
 
     
 def makeView() :
